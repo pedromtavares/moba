27ce89b3e46493f8af1b2b000c58bbdd597f48d0
Pedro Mateus Tavares <pedromateustavares@gmail.com>
2021-04-25 23:27:32 -0300

    Updated tests

--- test/moba/conductor_test.exs
+++ test/moba/conductor_test.exs
@@ -28,14 +28,14 @@ defmodule Moba.ConductorTest do
 
       match = Conductor.server_update!(current)
 
-      assert Date.compare(match.last_server_update_at, current.last_server_update_at) == :gt
+      assert Date.compare(match.last_server_update_at, DateTime.utc_now()) == :eq
     end
 
     test "new_pvp_round!" do
       current = Moba.current_match()
       match = Conductor.new_pvp_round!(current)
 
-      assert Date.compare(match.last_pvp_round_at, current.last_pvp_round_at) == :gt
+      assert Date.compare(match.last_pvp_round_at, DateTime.utc_now()) == :eq
     end
   end
 end
