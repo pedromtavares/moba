4f8626ff521628b4828614a7d0d6c97389c84e2a
Pedro Tavares <pedromateustavares@gmail.com>
2022-02-24 00:20:50 -0300

    Only allow shard buybacks for higher pve tiers

--- lib/moba_web/views/jungle_view.ex
+++ lib/moba_web/views/jungle_view.ex
@@ -10,6 +10,10 @@ defmodule MobaWeb.JungleView do
     boss.total_hp * 100 / boss.avatar.total_hp
   end
 
+  def can_shard_buyback?(%{user: user} = hero) do
+    hero.pve_tier > 3 && hero.league_tier < Moba.master_league_tier() && user.shard_count >= Accounts.shard_buyback_price(user)
+  end
+
   def farming_container_background(hero, assigns) do
     if farming_progression(hero, assigns) >= 100 do
       "rgba(54, 64, 74, 0.8)"
