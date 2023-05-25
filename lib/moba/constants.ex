dbdd9382669d5c419a292256f8a2b2c9f2e79265
Pedro Tavares <pedromateustavares@gmail.com>
2023-05-25 19:18:36 -0300

    deflation is just better

--- lib/moba/constants.ex
+++ lib/moba/constants.ex
@@ -190,7 +190,7 @@ defmodule Moba.Constants do
 
       def victory_match_points(diff), do: ceil(diff * 0.05)
 
-      def defeat_match_points(diff), do: floor(victory_match_points(-diff) * 1.1)
+      def defeat_match_points(diff), do: ceil(victory_match_points(-diff) * 1.1)
     end
   end
 end
