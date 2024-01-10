5689b6dae296a02749133a955cffb917314698b0
Pedro Tavares <pedromateustavares@gmail.com>
2024-01-10 20:41:29 -0300

    Move nostrum back to config

--- config/config.exs
+++ config/config.exs
@@ -31,6 +31,9 @@ config :ueberauth, Ueberauth.Strategy.Discord.OAuth,
   client_id: System.get_env("DISCORD_CLIENT_ID"),
   client_secret: System.get_env("DISCORD_CLIENT_SECRET")
 
+config :nostrum,
+  token: System.get_env("DISCORD_BOT_TOKEN")
+
 config :torch,
   otp_app: :moba,
   template_format: "eex"
